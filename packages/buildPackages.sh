07642cf033a68bd47cedf4a418df5ec3cb87fdf1
Matt Marum <mmarum@sugarcrm.com>
2016-11-14 15:36:32 -0500

    fix buildPackages.sh script

--- packages/buildPackages.sh
+++ packages/buildPackages.sh
@@ -3,16 +3,17 @@
 
 for i in * ; do
   if [ -d "$i" ]; then
+  	echo "building $i"
     cd "$i"
     if [ -f "manifest.php" ]
     then
-      zip -r --filesync ../$i.zip * -x "*.DS_Store" -x ".git*" -x "__MAC*"
-      cd ..
-    else
-      if [ -f "pack.php" ]
-      then
-        php pack.php "1.0.0"
-      fi
+      zip -r --filesync ../$i.zip * -x "*.DS_Store" -x ".git*" -x "__MAC*"      
+   else
+     if [ -f "pack.php" ]
+     then
+       php pack.php "1.0.0"
+     fi
     fi
+    cd ..
   fi
 done
