ff66795456895856d21864991855ea7e8a40d1ac
Matt Marum <mmarum@sugarcrm.com>
2021-01-20 16:19:18 -0500

    Add new FloatingDivAction example for 10.2 and up

---
+++ packages/FloatingDivAction-10.2+/src/custom/clients/base/views/click-to-call/click-to-call.php
@@ -0,0 +1,6 @@
+<?php
+// Copyright 2016 SugarCRM Inc.  Licensed by SugarCRM under the Apache 2.0 license.
+$viewdefs["base"]["view"]["click-to-call"] = array(
+    // iframe source to use inside the Popup
+    'iframeSrc' => '//httpbin.org/get',
+);
